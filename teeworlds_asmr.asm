b2f04552b475cb2ad38f663f14a8733e0d8b1a81
ChillerDragon <ChillerDragon@gmail.com>
2023-09-10 10:10:45 +0200

    Do not touch the stack pointer

--- teeworlds_asmr.asm
+++ teeworlds_asmr.asm
@@ -175,7 +175,6 @@ print_uint32:
     mov    ecx, 0xa              ; base 10
     push   rcx                   ; ASCII newline '\n' = 0xa = base
     mov    rsi, rsp
-    sub    rsp, 16               ; not needed on 64-bit Linux, the red-zone is big enough.  Change the LEA below if you remove this.
 
 ;;; rsi is pointing at '\n' on the stack, with 16B of "allocated" space below that.
 .print_uint32_toascii_digit:                ; do {
@@ -194,13 +193,12 @@ print_uint32:
     mov    eax, SYS_WRITE
     mov    edi, STDOUT
     ; pointer already in RSI    ; buf = last digit stored = most significant
-    lea    edx, [rsp+16 + 1]    ; yes, it's safe to truncate pointers before subtracting to find length.
+    lea    edx, [rsp+1]    ; yes, it's safe to truncate pointers before subtracting to find length.
     sub    edx, esi             ; RDX = length = end-start, including the \n
     syscall                     ; write(1, string /*RSI*/,  digits + 1)
 
-    add  rsp, 24                ; (in 32-bit: add esp,20) undo the push and the buffer reservation
-
-    pop rcx
+    pop rcx ; revert our newline push
+    pop rcx ; restore rcx from callee
     pop rsi
     pop rax
     ret
