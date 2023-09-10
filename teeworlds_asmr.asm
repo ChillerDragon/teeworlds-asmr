ce6d04bd1340dd8b0522ca5b9628bc6cc07c66d4
ChillerDragon <ChillerDragon@gmail.com>
2023-09-10 14:12:56 +0200

    Adapt insane assembly to be closer to what objdump disassembles
    Makes debugging with gdb and objdump easier. To find this snippet in the
    code base again.
    
    nasm does some optimizations and does not use the full register when
    we only store small values in it.
    
    Also gdb and objdump represent values as hex not as decimal.
    So if we also use hex in the code it makes searching the objdump/gdb
    code in the source code easier.

--- teeworlds_asmr.asm
+++ teeworlds_asmr.asm
@@ -246,30 +246,30 @@ print_menu:
 
 insane_console:
     ; fetch the current terminal settings
-    mov rax, 16 ; __NR_ioctl
-    mov rdi, 0 ; fd: stdin
-    mov rsi, 21505 ; cmd: TCGETS
+    mov eax, 0x10 ; __NR_ioctl
+    mov edi, 0x0 ; fd: stdin
+    mov esi, 0x5401 ; cmd: TCGETS
     mov rdx, orig ; arg: the buffer, orig
     syscall
     ; agian, but this time for the 'new' buffer
-    mov rax, 16
-    mov rdi, 0
-    mov rsi, 21505
+    mov rax, 0x10
+    mov rdi, 0x0
+    mov rsi, 0x5401
     mov rdx, new
     syscall
     ; change settings
     ; ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON)
-    and dword [new+0], -1516
+    and dword [new+0], 0xfffffa14 ; -1516
     ; ~OPOST
-    and dword [new+4], -2
+    and dword [new+4], 0xfffffffe ; -2
     ; ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN)
-    and dword [new+12], -32844
+    and dword [new+12], 0xffff7fb4 ; -32844
     ; ~(CSIZE | PARENB)
-    and dword [new+8], -305
+    and dword [new+8], 0xfffffecf ; -305
     ; set settings (with ioctl again)
-    mov rax, 16 ; __NR_ioctl
-    mov rdi, 0 ; fd: stdin
-    mov rsi, 21506 ; cmd: TCSETS
+    mov rax, 0x10 ; __NR_ioctl
+    mov rdi, 0x0 ; fd: stdin
+    mov rsi, 0x5402 ; cmd: TCSETS
     mov rdx, new ; arg: the buffer, new
     syscall
     ret
