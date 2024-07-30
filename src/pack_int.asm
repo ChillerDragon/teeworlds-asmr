07878cf7fde0e245363fe775dc327f22dcfea45f
ChillerDragon <ChillerDragon@gmail.com>
2024-07-30 21:17:11 +0800

    pack single and double byte positive ints

--- src/pack_int.asm
+++ src/pack_int.asm
@@ -2,10 +2,50 @@ pack_int:
     ; pack_int [rax] [rdi]
     ;  rax = integer
     ;  rdi = buffer to write to
-    ; returns the number of bytes written to rax
+    ; returns offsetted pointer in rax
     push_registers
 
-    mov byte [rdi], al
+    mov byte [rdi], 0
+
+    ; todo: negative
+
+    ; pack 6 bit into dst
+    mov rcx, 0
+    mov ecx, eax
+    and ecx, 0b0011_1111
+    mov byte dl, [rdi]
+    or dl, cl
+    mov byte [rdi], dl
+
+    ; discard 6 bits
+    shr eax, 6
+
+.pack_int_loop:
+    cmp eax, 0
+    je .pack_int_end
+
+    ; set extended bit
+    mov byte dl, [rdi]
+    or dl, 0b1000_0000
+    mov byte [rdi], dl
+
+    ; increment pointer
+    inc rdi
+
+    ; pack 7 bits
+    mov ecx, eax
+    and ecx, 0b0111_1111
+    mov byte [rdi], cl
+
+    ; discard 7 bits
+    shr eax, 7
+
+    jmp .pack_int_loop
+.pack_int_end:
+
+    ; increment pointer
+    inc rdi
 
     pop_registers
     ret
+
--- tests/pack_int_test.asm
+++ tests/pack_int_test.asm
@@ -1,12 +1,37 @@
 %include "tests/assert.asm"
 
 _start:
-    ; call to be tested function
-    mov rax, 1
+test_pack_int_single_positive:
+    mov rax, 9
     mov rdi, assert_input_buf
     call pack_int
-
     mov al, [assert_input_buf]
-    assert_al_eq 0x01
+    assert_al_eq 9
+
+    mov rax, 2
+    mov rdi, assert_input_buf
+    call pack_int
+    mov al, [assert_input_buf]
+    assert_al_eq 2
+
+    mov rax, 63
+    mov rdi, assert_input_buf
+    call pack_int
+    mov al, [assert_input_buf]
+    assert_al_eq 63
+
+test_pack_int_double_positive:
+    mov rax, 64
+    mov rdi, assert_input_buf
+    call pack_int
+    mov eax, [assert_input_buf]
+    assert_eax_eq 0x01_80 ; 0x80 0x81 is the sane people endianness
+
+    mov rax, 65
+    mov rdi, assert_input_buf
+    call pack_int
+    mov eax, [assert_input_buf]
+    assert_eax_eq 0x01_81 ; 0x81 0x01 is the sane people endianness
+
     exit 0
 
