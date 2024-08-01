d319fd11aaecedea7adb49519e16124bde4ca7f9
ChillerDragon <ChillerDragon@gmail.com>
2024-08-01 09:45:07 +0800

    Verify case sensitivity

--- tests/str_cmp_test.asm
+++ tests/str_cmp_test.asm
@@ -56,5 +56,23 @@ test_str_comp_different_length:
     call str_comp
     assert_is_false
 
+test_str_comp_different_casing:
+    ; prepare
+    assert_input_buf_reset
+    assert_input_buf_push_byte 'A'
+    assert_input_buf_push_byte 'A'
+    assert_input_buf_push_byte 'A'
+    assert_input_buf_push_byte 0x00
+    assert_input_buf_push_byte 'A'
+    assert_input_buf_push_byte 'a'
+    assert_input_buf_push_byte 'A'
+    assert_input_buf_push_byte 0x00
+
+    ; call to be tested function
+    mov rax, assert_input_buf
+    lea rdi, [assert_input_buf + 4]
+    call str_comp
+    assert_is_false
+
     exit 0
 
