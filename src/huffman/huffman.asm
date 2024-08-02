379a586d4d48bf2bcfa8cc593c558066b23599cb
ChillerDragon <ChillerDragon@gmail.com>
2024-08-02 16:39:07 +0800

    Make up some convention for subdir includes
    Don't call it main, index, or init
    Match the folder name
    
    Don't put any code in (maybe idk xd)

---
+++ src/huffman/huffman.asm
@@ -0,0 +1,3 @@
+%include "src/huffman/bubble_sort.asm"
+%include "src/huffman/set_bits_r.asm"
+%include "src/huffman/construct_tree.asm"
--- src/teeworlds_asmr.asm
+++ src/teeworlds_asmr.asm
@@ -139,9 +139,7 @@ section .text
 %include "src/on_game.asm"
 %include "src/packet_packer.asm"
 %include "src/pack_int.asm"
-%include "src/huffman/bubble_sort.asm"
-%include "src/huffman/set_bits_r.asm"
-%include "src/huffman/construct_tree.asm"
+%include "src/huffman/huffman.asm"
 
 print_udp:
     print s_got_udp
