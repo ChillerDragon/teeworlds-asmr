5ac3cae8a79a629255c1e0adbacda86ef027bf78
ChillerDragon <ChillerDragon@gmail.com>
2024-08-04 16:38:07 +0800

    Comments with nodes array comparison to working python code

--- src/huffman/construct_tree.asm
+++ src/huffman/construct_tree.asm
@@ -119,6 +119,16 @@ _huff_construct_tree:
     mov ax, [rsi + HUFF_CCONSTRUCTION_NODE_NODE_ID_OFFSET]
     mov word [huff_nodes + rbx + HUFF_CNODE_LEAF_0_OFFSET], ax
 
+    ; call print_int32
+    ; call print_space
+    ; matches the python version
+    ; 256 257 258 259 260 261 262 263 264 265 266 267 268 269 270 271 272 273 274 275 276 277 278 279 280 281 282 283 284 285 286 287 288 289 290 291 292 293 294 295 296 29
+    ; 7 298 299 300 301 302 303 304 305 306 307 308 309 310 311 312 313 314 315 316 317 318 319 320 321 322 323 324 325 326 327 328 329 330 331 332 333 334 335 336 337 338 339 340 341 342 343 344 345 346 347 348 349 3
+    ; 50 351 352 353 354 355 356 357 358 359 360 361 362 363 364 365 366 367 368 369 370 371 372 373 374 375 376 377 378 379 380 381 382 383 384 385 386 387 388 389 390 391 392 393 394 395 396 397 398 399 400 401 402 
+    ; 403 404 405 406 407 408 409 410 411 412 413 414 415 416 417 418 419 420 421 422 423 424 425 426 427 428 429 430 431 432 433 434 435 436 437 438 439 440 441 442 443 444 445 446 447 448 449 450 451 452 453 454 455
+    ; 456 457 458 459 460 461 462 463 464 465 466 467 468 469 470 471 472 473 474 475 476 477 478 479 480 481 482 483 484 485 486 487 488 489 490 491 492 493 494 495 496 497 498 499 500 501 502 503 504 505 506 507 50
+    ; 8 509 510 511
+
     ; m_aNodes[m_NumNodes].m_aLeafs[1] = apNodesLeft[NumNodesLeft-2]->m_NodeId;
     ; rax = apNodesLeft[NumNodesLeft-2]->m_NodeId;
     ;                 NumNodesLeft - 2
@@ -184,6 +194,54 @@ _huff_construct_tree:
     lea rax, [huff_nodes + ebx]
     ; m_pStartNode = &m_aNodes[m_NumNodes-1];
     mov qword [huff_start_node], rax
+
+    call huff_print_arr_nodes
+
+    ; asmr
+    ; {bits: -0, NumBits: -1, Leafs: [4294967295, -4294901761], Symbol: -0}
+    ; {bits: -0, NumBits: -1, Leafs: [4294967295, 131071], Symbol: 1}
+    ; {bits: 0, NumBits: 0, Leafs: [511, 131072], Symbol: 2}
+    ; {bits: 0, NumBits: 0, Leafs: [66046, 196609], Symbol: 3}
+    ; {bits: 0, NumBits: 0, Leafs: [131581, 262146], Symbol: 4}
+    ; {bits: 0, NumBits: 0, Leafs: [197116, 327683], Symbol: 5}
+    ; [..]
+    ; {bits: 0, NumBits: 0, Leafs: [16253191, 16384248], Symbol: 250}
+    ; {bits: 0, NumBits: 0, Leafs: [16318726, 16449785], Symbol: 251}
+    ; {bits: 0, NumBits: 0, Leafs: [16384261, 16515322], Symbol: 252}
+    ; {bits: 0, NumBits: 0, Leafs: [16449796, 16580859], Symbol: 253}
+    ; {bits: 0, NumBits: 0, Leafs: [16515331, 16646396], Symbol: 254}
+    ; {bits: 0, NumBits: 0, Leafs: [16580866, 16711933], Symbol: 255}
+    ; {bits: 0, NumBits: 0, Leafs: [16646401, 254], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [16711936, 255], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+    ; {bits: 0, NumBits: 0, Leafs: [0, 0], Symbol: 0}
+
+    ; pyson:
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 0>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 1>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 2>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 3>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 4>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 5>
+    ; [..]
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 252>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 253>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 254>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 255>
+    ; <bits: 0, NumBits: 4294967295, Leafs: [65535, 65535], Symbol: 256>
+    ; <bits: 0, NumBits: 0, Leafs: [256, 119], Symbol: 0>
+    ; <bits: 0, NumBits: 0, Leafs: [257, 248], Symbol: 0>
+    ; <bits: 0, NumBits: 0, Leafs: [108, 126], Symbol: 0>
+    ; <bits: 0, NumBits: 0, Leafs: [118, 240], Symbol: 0>
+    ; <bits: 0, NumBits: 0, Leafs: [122, 99], Symbol: 0>
+
+    exit 0
 
     ;  rax = *pNode (still set)
     ;  rdi = int Bits
