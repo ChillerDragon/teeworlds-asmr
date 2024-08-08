890a85aeeeb35b9378aa7baa0e228de5a678641b
ChillerDragon <ChillerDragon@gmail.com>
2024-08-08 08:48:53 +0800

    Fix nodes left size (it is an array of pointers)

--- src/bss/huffman.asm
+++ src/bss/huffman.asm
@@ -5,7 +5,7 @@ huff_nodes resb HUFF_CNODE_SIZE * HUFFMAN_MAX_NODES
 huff_nodes_left_storage resb HUFF_CNODE_SIZE * HUFFMAN_MAX_SYMBOLS
 
 ; *apNodesLeft[HUFFMAN_MAX_SYMBOLS];
-huff_nodes_left resb 4 * HUFFMAN_MAX_SYMBOLS
+huff_nodes_left resb 8 * HUFFMAN_MAX_SYMBOLS
 
 ; m_apDecodeLut array of CNODE structs
 huff_decode_lut resb HUFFMAN_LUTSIZE * 8
