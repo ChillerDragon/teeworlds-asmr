8e32f9b35887bdc0b1a540a07e44e72e24654c27
ChillerDragon <ChillerDragon@gmail.com>
2024-08-11 12:19:29 +0800

    Fix construction node array size

--- src/bss/huffman.asm
+++ src/bss/huffman.asm
@@ -2,12 +2,12 @@
 huff_nodes resb HUFF_CNODE_SIZE * HUFFMAN_MAX_NODES
 
 ; aNodesLeftStorage[HUFFMAN_MAX_SYMBOLS];
-huff_nodes_left_storage resb HUFF_CNODE_SIZE * HUFFMAN_MAX_SYMBOLS
+huff_nodes_left_storage resb HUFF_CCONSTRUCTION_NODE_SIZE * HUFFMAN_MAX_SYMBOLS
 
 ; *apNodesLeft[HUFFMAN_MAX_SYMBOLS];
 huff_nodes_left resb 8 * HUFFMAN_MAX_SYMBOLS
 
-; m_apDecodeLut array of CNODE structs
+; m_apDecodeLut array of CNODE struct pointers
 huff_decode_lut resb HUFFMAN_LUTSIZE * 8
 
 ; pointer to start node
