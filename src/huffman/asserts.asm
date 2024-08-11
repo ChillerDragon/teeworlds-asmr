bb07344d412731bf99239ad87353ebc73079551d
ChillerDragon <ChillerDragon@gmail.com>
2024-08-11 12:18:02 +0800

    Add huff storage arr assert macro

--- src/huffman/asserts.asm
+++ src/huffman/asserts.asm
@@ -5,3 +5,10 @@
     pop rax
 %endmacro
 
+%macro huff_assert_nodes_left_storage_ptr 1
+    push rax
+    mov rax, %1
+    check_bounds rax, huff_nodes_left_storage, HUFF_CCONSTRUCTION_NODE_SIZE, HUFFMAN_MAX_SYMBOLS
+    pop rax
+%endmacro
+
