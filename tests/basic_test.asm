337da5e4c479140e0941beaa9e418b9cd8b3990b
ChillerDragon <ChillerDragon@gmail.com>
2024-08-12 11:22:46 +0800

    Use basic test to learn division

--- tests/basic_test.asm
+++ tests/basic_test.asm
@@ -1,7 +1,41 @@
 %include "tests/assert.asm"
 
 _start:
-    mov eax, 0xAABB
-    assert_eax_eq 0xAABB
+_test_div_10_by_5:
+    ; clear dividend remainder thing
+    mov rdx, 0
+
+    ; dividend
+    mov eax, 10
+
+    ; divisor
+    mov ecx, 5
+
+    div ecx
+
+    ; result is stored in eax (always)
+    ; remainder is stored in edx (always)
+    assert_eax_eq 2
+    mov eax, edx
+    assert_eax_eq 0
+
+_test_div_10_by_8:
+    ; clear dividend remainder thing
+    mov rdx, 0
+
+    ; dividend
+    mov eax, 10
+
+    ; divisor
+    mov ecx, 8
+
+    div ecx
+
+    ; result is stored in eax (always)
+    ; remainder is stored in edx (always)
+    assert_eax_eq 1
+    mov eax, edx
+    assert_eax_eq 2
+
     exit 0
 
