018baf5e2c4f6f663db689e607c34a9dcd1e3c36
ChillerDragon <ChillerDragon@gmail.com>
2024-08-13 16:03:17 +0800

    More decompress code

--- src/huffman/decompress.asm
+++ src/huffman/decompress.asm
@@ -4,6 +4,16 @@
 %define S_EOF 16
 %define S_NODE 8
 
+%macro huff_set_node_decode_lut_bits_lutmask 0
+    ; pNode = m_apDecodeLut[Bits&HUFFMAN_LUTMASK];
+    mov rbx, 0
+    mov ebx, dword [rbp-S_BITS]
+    and ebx, HUFFMAN_LUTMASK
+    imul rbx, 8
+    mov r9, [huff_decode_lut + rbx]
+    mov qword [rbp-S_NODE], r9
+%endmacro
+
 huff_decompress:
     ; huff_decompress [rax] [rdi] [rsi] [rdx]
     ;  rax = input
@@ -42,6 +52,7 @@ huff_decompress:
     mov qword [rbp-S_EOF], r9
 
 .huff_decompress_outer_while:
+    ; {A} try to load a node now, this will reduce dependency at location {D}
     ; pNode = nullptr
     mov qword [rbp-S_NODE], 0
 
@@ -49,13 +60,7 @@ huff_decompress:
     jl .huff_decompress_skip_node_set
     .huff_decompress_set_node:
     ; pNode = m_apDecodeLut[Bits&HUFFMAN_LUTMASK];
-    mov rbx, 0
-    mov ebx, dword [rbp-S_BITS]
-    and ebx, HUFFMAN_LUTMASK
-    imul rbx, 8
-    mov r9, [huff_decode_lut + rbx]
-    huff_assert_nodes_ptr r9, __LINE__, __FILE__
-    mov qword [rbp-S_NODE], r9
+    huff_set_node_decode_lut_bits_lutmask
     .huff_decompress_skip_node_set:
 
     .huff_decompress_fill_while:
@@ -65,14 +70,61 @@ huff_decompress:
     je .huff_decompress_fill_while_end
 
     ; Bits |= (*pSrc++) << Bitcount;
+    mov r9, 0
+    mov r9b, byte [rax]
+    mov r8, 0
+    mov r9d, dword [rbp-S_BITCOUNT]
+    shift_left r9, r8
+    ; r9 = (*pSrc++) << Bitcount;
+    mov r8, 0
+    mov r8d, dword [rbp-S_BITS]
+    or r8, r9
+    mov dword [rbp-S_BITS], r8d
+    ; pSrc++
+    inc rax
 
-
+    add dword [rbp-S_BITCOUNT], 8
+    jmp .huff_decompress_fill_while
     .huff_decompress_fill_while_end:
+
+    ; {C} load symbol now if we didn't that earlier at location {A}
+    cmp qword [rbp-S_NODE], 0
+    jne .huff_decompress_c_skip_set_node
+    .huff_decompress_c_set_node:
+    ; pNode = m_apDecodeLut[Bits&HUFFMAN_LUTMASK];
+    huff_set_node_decode_lut_bits_lutmask
+    .huff_decompress_c_skip_set_node:
+
+    cmp qword [rbp-S_NODE], 0
+    je .huff_decompress_error_c_no_node
+
+    ; {D} check if we hit a symbol already
+    mov r10, qword [rbp-S_NODE]
+    huff_assert_nodes_ptr r10, __LINE__, __FILE__
+    mov r8, 0
+    mov r8d, dword [r10 + HUFF_CNODE_NUM_BITS_OFFSET]
+    cmp r8d, 0
+    je .huff_decompress_remove_the_bits_that_the_lut_checked_up_for_us
+    .huff_decompress_remove_the_bits_for_that_symbol:
+        ; Bits >>= pNode->m_NumBits;
+
+    jmp .huff_decompress_check_for_eof
+    ; } else {
+    .huff_decompress_remove_the_bits_that_the_lut_checked_up_for_us:
 
     .huff_decompress_walk_while:
     .huff_decompress_walk_while_end:
+
+    ; } // end of outer if(pNode->m_NumBits) if statament
+    .huff_decompress_check_for_eof:
+
 .huff_decompress_outer_while_end:
 
+    jmp .huff_decompress_end
+.huff_decompress_error_c_no_node:
+    puts "[error] huffman got node nullptr in {C} section"
+    exit 1
+.huff_decompress_end:
     mov rsp, rbp
 
     pop_registers
