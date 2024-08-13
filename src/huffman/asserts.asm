e1f75c70bd0fd8f4baf903e04634a52b6cc605b1
ChillerDragon <ChillerDragon@gmail.com>
2024-08-13 14:28:13 +0800

    Assert that the first 3 decode lut elements are valid nodes pointer

--- src/huffman/asserts.asm
+++ src/huffman/asserts.asm
@@ -32,6 +32,40 @@
     pop rax
 %endmacro
 
+%macro huff_assert_decode_lut_ptr 3
+    ; huff_assert_decode_lut_ptr [ptr] [__LINE__] [__FILE__]
+    push rax
+    push rbp
+    push rdi
+    push r9
+
+    ; crazy hack to store %1 on the stack
+    ; before the stack allocated string
+    ; and pop it back with a unknown stack offset
+    mov rax, %1
+    push rax
+    mov r9, rsp
+
+    str_to_stack %3
+    mov rdi, rax ; rdi is ptr to file name str
+
+    ; soft pop rax of the hacked stack
+    mov rax, [r9]
+
+    check_bounds rax, huff_decode_lut, 8, HUFFMAN_LUTSIZE, %2, rdi
+
+    ; free stack str
+    mov rsp, rbp
+
+    ; free ptr hack
+    pop rax
+
+    pop r9
+    pop rdi
+    pop rbp
+    pop rax
+%endmacro
+
 %macro huff_assert_nodes_left_storage_ptr 3
     ; huff_assert_nodes_left_storage_ptr [ptr] [__LINE__] [__FILE__]
     push rax
--- tests/huffman_init_test.asm
+++ tests/huffman_init_test.asm
@@ -2,5 +2,27 @@
 
 _start:
     call huff_init
+
+
+    lea rax, [huff_decode_lut + 0]
+    huff_assert_decode_lut_ptr rax, __LINE__, __FILE__
+
+    mov rax, [rax]
+    huff_assert_nodes_ptr rax, __LINE__, __FILE__
+
+
+    lea rax, [huff_decode_lut + 8]
+    huff_assert_decode_lut_ptr rax, __LINE__, __FILE__
+
+    mov rax, [rax]
+    huff_assert_nodes_ptr rax, __LINE__, __FILE__
+
+
+    lea rax, [huff_decode_lut + 16]
+    huff_assert_decode_lut_ptr rax, __LINE__, __FILE__
+
+    mov rax, [rax]
+    huff_assert_nodes_ptr rax, __LINE__, __FILE__
+
     exit 0
 
