036ce77b31db3a2671d3f95e1890b50eef6183f9
ChillerDragon <ChillerDragon@gmail.com>
2024-08-13 11:37:36 +0800

    Fix oob tests

--- tests/array_oob_test.asm
+++ tests/array_oob_test.asm
@@ -1,25 +1,32 @@
 %include "tests/assert.asm"
 
 _start:
+    str_to_stack __FILE__
+    mov r9, rax
+
     ; rax is pointer to 1st element in the array
     mov rax, huff_nodes
-    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, __FILE__
+    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, r9
 
     ; rax is pointer to 2nd element in the array
     mov rax, huff_nodes
     add rax, HUFF_CNODE_SIZE
-    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, __FILE__
+    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, r9
 
     ; rax is pointer to 3rd element in the array
     mov rax, huff_nodes
     add rax, HUFF_CNODE_SIZE
     add rax, HUFF_CNODE_SIZE
-    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, __FILE__
+    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, r9
 
     ; rax is pointer to the last element in the array
     mov rax, HUFF_CNODE_SIZE
     imul rax, HUFFMAN_MAX_NODES
     add rax, huff_nodes
-    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, __FILE__
+    check_bounds rax, huff_nodes, HUFF_CNODE_SIZE, HUFFMAN_MAX_NODES, __LINE__, r9
+
+    ; free stack str
+    mov rsp, rbp
+
     exit 0
 
