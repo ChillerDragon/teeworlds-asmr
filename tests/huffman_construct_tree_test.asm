9a155f71b96ce2cfef766d1813b36f0a7b809d7c
ChillerDragon <ChillerDragon@gmail.com>
2024-08-14 14:30:30 +0800

    Some of the nodes match fully

--- tests/huffman_construct_tree_test.asm
+++ tests/huffman_construct_tree_test.asm
@@ -8,7 +8,88 @@ _test_call_huff_construct_tree_assert_start_node:
     mov rax, qword [huff_start_node]
     huff_assert_nodes_ptr rax, __LINE__, __FILE__
 
-_test_call_huff_construct_tree_assert_nodes:
+
+_test_call_huff_construct_tree_assert_node_0:
+    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 0)]
+    ; mov rax, rsi
+    ; call huff_print_struct_cnode
+
+    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
+    ; assert_eax_eq 1
+    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
+    ; assert_eax_eq 1
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
+    ; assert_eax_eq 0
+
+_test_call_huff_construct_tree_assert_node_1:
+    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 1)]
+    ; mov rax, rsi
+    ; call huff_print_struct_cnode
+
+    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
+    assert_eax_eq 8
+    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
+    assert_eax_eq 4
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
+    assert_eax_eq 1
+
+_test_call_huff_construct_tree_assert_node_2:
+    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 2)]
+    ; mov rax, rsi
+    ; call huff_print_struct_cnode
+
+    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
+    assert_eax_eq 2
+    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
+    assert_eax_eq 5
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
+    assert_eax_eq 2
+
+_test_call_huff_construct_tree_assert_node_3:
+    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 3)]
+    ; mov rax, rsi
+    ; call huff_print_struct_cnode
+
+    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
+    assert_eax_eq 22
+    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
+    assert_eax_eq 8
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
+    assert_eax_eq 65535
+    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
+    assert_eax_eq 3
+
+_test_call_huff_construct_tree_assert_node_510:
+    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 510)]
+    ; mov rax, rsi
+    ; call huff_print_struct_cnode
+
+    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
+    ; assert_eax_eq 0 ; we get 2
+    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
+    ; assert_eax_eq 0
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
+    assert_eax_eq 507
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
+    assert_eax_eq 508
+    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
+    assert_eax_eq 0
+
+_test_call_huff_construct_tree_assert_node_511:
     lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 511)]
     ; mov rax, rsi
     ; call huff_print_struct_cnode
