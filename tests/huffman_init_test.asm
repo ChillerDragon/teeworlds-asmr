c4844d09afb120e201de3c331f31f94a83bd2e66
ChillerDragon <ChillerDragon@gmail.com>
2024-08-14 14:16:39 +0800

    Add failing tests

--- tests/huffman_init_test.asm
+++ tests/huffman_init_test.asm
@@ -2,6 +2,7 @@
 
 _start:
 _test_huff_init:
+    call print_newline ; todo: remove
     call huff_init
 
 
@@ -47,6 +48,24 @@ _test_huff_init_assert_eof_node:
     assert_eax_eq 65535
     movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
     assert_eax_eq 0
+
+_test_huff_init_assert_lut_nodes:
+
+    ; ; make sure that decode lut points to same nodes like C++
+    ; ; implementation. Tested calling this in the end of the C++
+    ; ; Init() method:
+    ; ;  PrintNode(m_apDecodeLut[0]);
+    ; mov rsi, [huff_decode_lut + (8 * 0)]
+    ; mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
+    ; assert_eax_eq 0
+    ; mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
+    ; assert_eax_eq 5
+    ; movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
+    ; assert_eax_eq 65535
+    ; movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
+    ; assert_eax_eq 65535
+    ; movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
+    ; assert_eax_eq 128
 
     exit 0
 
