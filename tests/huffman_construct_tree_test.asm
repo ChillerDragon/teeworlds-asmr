32e64569e09f1835af7fa2e2a14761dd0a143ffb
ChillerDragon <ChillerDragon@gmail.com>
2024-08-15 09:56:39 +0800

    Use macro and test more elements in the nodes array

--- tests/huffman_construct_tree_test.asm
+++ tests/huffman_construct_tree_test.asm
@@ -1,5 +1,22 @@
 %include "tests/assert.asm"
 
+%macro test_assert_node 12
+    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * %2)]
+    ; mov rax, rsi
+    ; call huff_print_struct_cnode
+
+    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
+    assert_eax_eq %4
+    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
+    assert_eax_eq %6
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
+    assert_eax_eq %8
+    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
+    assert_eax_eq %10
+    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
+    assert_eax_eq %12
+%endmacro
+
 _start:
 _test_call_huff_construct_tree:
     call print_newline ; todo: remove
@@ -9,134 +26,65 @@ _test_call_huff_construct_tree_assert_start_node:
     mov rax, qword [huff_start_node]
     huff_assert_nodes_ptr rax, __LINE__, __FILE__
 
-
 _test_call_huff_construct_tree_assert_node_0:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 0)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
-
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 1
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 1
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 0
+    test_assert_node i, 0, bits, 1, num_bits, 1, leaf0, 65535, leaf1, 65535, symbol, 0
 
 _test_call_huff_construct_tree_assert_node_1:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 1)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
-
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 8
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 4
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 1
+    test_assert_node i, 1, bits, 8, num_bits, 4, leaf0, 65535, leaf1, 65535, symbol, 1
 
 _test_call_huff_construct_tree_assert_node_2:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 2)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
-
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 2
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 5
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 2
+    test_assert_node i, 2, bits, 2, num_bits, 5, leaf0, 65535, leaf1, 65535, symbol, 2
 
 _test_call_huff_construct_tree_assert_node_3:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 3)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
-
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 22
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 8
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 3
+    test_assert_node i, 3, bits, 22, num_bits, 8, leaf0, 65535, leaf1, 65535, symbol, 3
 
 _test_call_huff_construct_tree_assert_node_4:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 4)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
-
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 30
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 6
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 4
+    test_assert_node i, 4, bits, 30, num_bits, 6, leaf0, 65535, leaf1, 65535, symbol, 4
 
 _test_call_huff_construct_tree_assert_node_5:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 5)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
-
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 118
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 7
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 5
+    test_assert_node i, 5, bits, 118, num_bits, 7, leaf0, 65535, leaf1, 65535, symbol, 5
 
 _test_call_huff_construct_tree_assert_node_6:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 6)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
-
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 54
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 8
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 6
+    test_assert_node i, 6, bits, 54, num_bits, 8, leaf0, 65535, leaf1, 65535, symbol, 6
 
 _test_call_huff_construct_tree_assert_node_7:
-    lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 7)]
-    ; mov rax, rsi
-    ; call huff_print_struct_cnode
+    test_assert_node i, 7, bits, 110, num_bits, 8, leaf0, 65535, leaf1, 65535, symbol, 7
 
-    mov eax, [rsi + HUFF_CNODE_BITS_OFFSET]
-    assert_eax_eq 110
-    mov eax, [rsi + HUFF_CNODE_NUM_BITS_OFFSET]
-    assert_eax_eq 8
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_0_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, word [rsi + HUFF_CNODE_LEAF_1_OFFSET]
-    assert_eax_eq 65535
-    movzx rax, byte [rsi + HUFF_CNODE_SYMBOL_OFFSET]
-    assert_eax_eq 7
+_test_call_huff_construct_tree_assert_node_8:
+    test_assert_node i, 8, bits, 4, num_bits, 5, leaf0, 65535, leaf1, 65535, symbol, 8
+
+_test_call_huff_construct_tree_assert_node_9:
+    test_assert_node i, 9, bits, 76, num_bits, 7, leaf0, 65535, leaf1, 65535, symbol, 9
+
+_test_call_huff_construct_tree_assert_node_10:
+    test_assert_node i, 10, bits, 122, num_bits, 7, leaf0, 65535, leaf1, 65535, symbol, 10
+
+_test_call_huff_construct_tree_assert_node_11:
+    test_assert_node i, 11, bits, 254, num_bits, 8, leaf0, 65535, leaf1, 65535, symbol, 11
+
+_test_call_huff_construct_tree_assert_node_12:
+    test_assert_node i, 12, bits, 114, num_bits, 7, leaf0, 65535, leaf1, 65535, symbol, 12
+
+_test_call_huff_construct_tree_assert_node_13:
+    test_assert_node i, 13, bits, 14, num_bits, 6, leaf0, 65535, leaf1, 65535, symbol, 13
+
+_test_call_huff_construct_tree_assert_node_14:
+    test_assert_node i, 14, bits, 244, num_bits, 8, leaf0, 65535, leaf1, 65535, symbol, 14
+
+_test_call_huff_construct_tree_assert_node_15:
+    test_assert_node i, 15, bits, 238, num_bits, 9, leaf0, 65535, leaf1, 65535, symbol, 15
+
+_test_call_huff_construct_tree_assert_node_16:
+    test_assert_node i, 16, bits, 106, num_bits, 7, leaf0, 65535, leaf1, 65535, symbol, 16
+
+_test_call_huff_construct_tree_assert_node_17:
+    test_assert_node i, 17, bits, 166, num_bits, 9, leaf0, 65535, leaf1, 65535, symbol, 17
+
+_test_call_huff_construct_tree_assert_node_18:
+    test_assert_node i, 18, bits, 92, num_bits, 7, leaf0, 65535, leaf1, 65535, symbol, 18
+
+_test_call_huff_construct_tree_assert_node_19:
+    test_assert_node i, 19, bits, 170, num_bits, 9, leaf0, 65535, leaf1, 65535, symbol, 19
 
 _test_call_huff_construct_tree_assert_node_510:
     lea rsi, [huff_nodes + (HUFF_CNODE_SIZE * 510)]
