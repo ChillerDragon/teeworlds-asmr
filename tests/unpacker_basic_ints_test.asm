10c10c27fd2de0022f098f0f85979ae6b00c9218
ChillerDragon <ChillerDragon@gmail.com>
2024-08-23 16:42:31 +0800

    The int unpacker seems to be solid

--- tests/unpacker_basic_ints_test.asm
+++ tests/unpacker_basic_ints_test.asm
@@ -14,8 +14,6 @@ _test_one_byte_ints:
     assert_eax_eq 2, __LINE__
 
 _test_negative_multi_byte:
-    call print_newline ; todo: remove
-
     mov byte [generic_buffer_128 + 0], 0xff
     mov byte [generic_buffer_128 + 1], 0x01
     unpacker_reset generic_buffer_128, 2
@@ -23,9 +21,7 @@ _test_negative_multi_byte:
     call get_int
     assert_eax_eq -128, __LINE__
 
-_test_positive_three_byte:
-    call print_newline ; todo: remove
-
+_test_negative_three_byte:
     mov byte [generic_buffer_128 + 0], 0xff
     mov byte [generic_buffer_128 + 1], 0xff
     mov byte [generic_buffer_128 + 2], 0x01
@@ -33,6 +29,27 @@ _test_positive_three_byte:
 
     call get_int
     assert_eax_eq -16384, __LINE__
+
+_test_positive_three_byte:
+    mov byte [generic_buffer_128 + 0], 0x80
+    mov byte [generic_buffer_128 + 1], 0xff
+    mov byte [generic_buffer_128 + 2], 0x01
+    unpacker_reset generic_buffer_128, 3
+
+    call get_int
+    assert_eax_eq 16320, __LINE__
+
+_test_positive_three_byte_plus_one_byte:
+    mov byte [generic_buffer_128 + 0], 0x80
+    mov byte [generic_buffer_128 + 1], 0xff
+    mov byte [generic_buffer_128 + 2], 0x01
+    mov byte [generic_buffer_128 + 3], 0x06
+    unpacker_reset generic_buffer_128, 4
+
+    call get_int
+    assert_eax_eq 16320, __LINE__
+    call get_int
+    assert_eax_eq 6, __LINE__
 
     exit 0
 
