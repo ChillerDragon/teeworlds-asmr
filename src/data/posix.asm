015b888a946591ab2d32e07a3842c759aecaeebc
ChillerDragon <chillerdragon@gmail.com>
2024-12-12 15:23:35 +0800

    Add helpers to append text to files

--- src/data/posix.asm
+++ src/data/posix.asm
@@ -4,8 +4,10 @@ STDOUT equ 1
 AF_INET      equ 0x2
 SOCK_DGRAM   equ 0x2
 MSG_DONTWAIT equ 64
+O_RDWR       equ 2
+O_CREAT      equ 64
+O_APPEND     equ 1024
 O_NONBLOCK   equ 2048
 F_SETFL      equ 4
 EWOULDBLOCK  equ 11
 EAGAIN       equ 11
-
--- src/syscalls.asm
+++ src/syscalls.asm
@@ -5,6 +5,88 @@
     syscall
 %endmacro
 
+write_str_to_file:
+    ; write_str_fo_file [rax] [rdi]
+    ;  rax = file descriptor
+    ;  rdi = null terminated string
+    ;
+    ; use `fopen` to get file descriptor
+    push_registers
+
+    ; file descriptor
+    mov r9, rax
+    ; string
+    mov r10, rdi
+    ; string length
+    mov rax, rdi
+    call str_length
+    mov r11, rax
+
+    mov rax, SYS_WRITE
+    mov rdi, r9 ; fd
+    mov rsi, r10 ; str
+    mov rdx, r11 ; string length
+    syscall
+
+    pop_registers
+    ret
+
+fopen:
+    ; fopen [rax] [rdi]
+    ;  rax = pointer to filename
+    ;  rdi = O_APPEND or 0
+    ;
+    ; returns into rax the file handle
+    ;
+    ; don't forget to call `close`
+    ;
+    ; example:
+    ;
+    ; mov rax, [label_with_str_filename]
+    ; mov rdi, O_APPEND
+    ; call fopen
+    ; call close
+    push_registers_keep_rax
+
+    ; filename
+    mov r9, rax
+    ; flags
+    mov r10, rdi
+
+    mov rax, SYS_OPEN
+
+    ; filename
+    mov rdi, r9
+
+    ; create file if is not there yet
+    mov rsi, r10
+    or rsi, O_RDWR
+    or rsi, O_CREAT
+
+    ; mode is determined by `umask`
+    mov rdx, 0666o
+    syscall
+
+    pop_registers_keep_rax
+    ret
+
+close:
+    ; close [rax]
+    ;  rax = file descriptor
+    push r9
+    push rdi
+    push rax
+
+    mov r9, rax
+    mov rax, SYS_CLOSE
+    mov rdi, r9
+    syscall
+
+    pop rax
+    pop rdi
+    pop r9
+    ret
+
 ; nanosleep [nanoseconds]
 %macro nanosleep 1
     push_registers
