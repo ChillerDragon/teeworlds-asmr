4893670e9b10777bf273342ba61ca09b58036bb7
ChillerDragon <chillerdragon@gmail.com>
2024-12-20 09:34:24 +0800

    Use OR instead of XOR for chunk header flags
    Not that it matters. Both work and I am pretty sure there is no
    difference. But OR seems simpler.
    We are ORing the flags onto two zero bits so the only case where
    XOR and OR are different can never happen here.

--- src/chunk_packer.asm
+++ src/chunk_packer.asm
@@ -64,13 +64,13 @@ pack_chunk_header:
     ; | 2 bits  | 6 bits  | 2 bits          | 6 bits | 8 bits          |
     ; +---------+---------+-----------------+--------+-----------------+
 
-    ; first byte is 2 bit flags (we xor them in)
+    ; first byte is 2 bit flags (we or them in)
     ; and the remaining 6 bit are the first part of size
     mov r8, rdi
     shr r8, 6
     and r8b, 0b00111111
     mov byte [rsi], r8b
-    xor byte [rsi], al
+    or byte [rsi], al
 
     ; second byte is only the size
     ; the sequence number will also be inserted later if it is vital
